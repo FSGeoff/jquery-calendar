464ad42a4d68116d4f238c213e47f0bc9906ede2
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-02 16:29:53 -0400

    added hours to divs using array/for loop

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	var amBox = $("#am-hours");
+	var pmBox = $("#pm-box");
+
+	var hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+	for (let i = 0; i <= hours.length; i++) {
+		// Create element
+		var amHours = $("<div>");
+		// Attach Content
+		amHours.addClass("sch-hrs day-hrs");
+		amHours.attr("value", hours[i]);
+		amHours.text(hours[i]);
+		console.log(amHours);
+		// Append to existing
+		$("#am-hours").append(amHours);
+	}
+
+	for (let i = 0; i <= hours.length; i++) {
+		// Create element
+		var pmHours = $("<div>");
+		// Attach Content
+		pmHours.addClass("sch-hrs night-hrs");
+		pmHours.attr("value", hours[i]);
+		pmHours.text(hours[i]);
+		console.log(pmHours);
+		// Append to existing
+		$("#pm-hours").append(pmHours);
+	}
+});

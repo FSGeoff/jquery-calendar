0f19e95ebe73cf0d916b82ce31a796a260ad903a
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-02 16:54:26 -0400

    aded text area to div

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 	var amBox = $("#am-hours");
 	var pmBox = $("#pm-box");
+	var currentDay = $("#currentDay");
 
 	var hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -12,8 +13,16 @@ $(document).ready(function () {
 		amHours.attr("value", hours[i]);
 		amHours.text(hours[i]);
 		console.log(amHours);
+
 		// Append to existing
 		$("#am-hours").append(amHours);
+
+		var event = $("<textarea>");
+
+		event.addClass("add-event");
+		event.attr("id", hours[i]);
+
+		amHours.append(event);
 	}
 
 	for (let i = 0; i <= hours.length; i++) {
@@ -24,7 +33,15 @@ $(document).ready(function () {
 		pmHours.attr("value", hours[i]);
 		pmHours.text(hours[i]);
 		console.log(pmHours);
+
 		// Append to existing
 		$("#pm-hours").append(pmHours);
+
+		var event = $("<textarea>");
+
+		event.addClass("add-event");
+		event.attr("id", hours[i]);
+
+		pmHours.append(event);
 	}
 });

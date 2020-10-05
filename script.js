9e785e528918a9ffbd18a49e8605d660b123045d
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-04 20:11:59 -0400

    added all buttons and moved to right

--- script.js
+++ script.js
@@ -6,30 +6,38 @@ $(document).ready(function () {
 	var currentDay = $("#currentDay");
 	var timeBlock = $("#time-block");
 	var eventBox = $("#event-box");
-	var eventBtn = $("#event-btn");
+
 	console.log(timeAndDate);
 	console.log(time);
 
 	var hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 	for (let i = 0; i < hours.length; i++) {
-		// Create element
+		// Create div, added content, then appended to pmBox
 		var amHours = $("<div>");
-		// Attach Content
 		amHours.addClass("sch-hrs day-hrs");
 		amHours.attr("value", hours[i]);
 		amHours.text(hours[i] + "AM");
-
-		// Append to existing
 		amBox.append(amHours);
 
+		// Create textarea, added content, then appended to pmBox
 		var event = $("<textarea>");
+		// Create button, add content then append to ...
+		var button = $("<button>");
+		button.css("class", "btn btn-info");
+		button.css("color", "blue");
+		button.css("float", "right");
+		button.text(hours[i]);
+		amBox.append(button);
+
 		event.css("background-color", "grey");
 		event.css("width", "100%");
 		event.addClass("add-event");
-		event.attr("id", hours[i]);
+		event.attr("id", "pm" + hours[i]);
 
 		amBox.append(event);
+
+		console.log(event);
 	}
 
 	for (let i = 0; i < hours.length; i++) {
@@ -53,14 +61,8 @@ $(document).ready(function () {
 		event.css("background-color", "grey");
 		event.css("width", "100%");
 		event.addClass("add-event");
-		event.attr("id", hours[i]);
+		event.attr("id", "pm" + hours[i]);
 		pmBox.append(event);
-
-		// // Create button, add content then append to ...
-		// var button = $("<button>");
-		// button.css("class", "btn btn-info");
-		// button.text(hours[i]);
-		// eventBtn.append(button);
 	}
 	amBox.on("click", function () {
 		saveEvent();

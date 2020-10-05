c2f498c386dc638fab764f8544480a055230288d
Geoff Nix <FullStackGeoff@gmail.com>
2020-10-05 17:15:01 -0400

    cleaned up code and adjusted time event color selector

--- script.js
+++ script.js
@@ -11,38 +11,49 @@ $(document).ready(function () {
 	$("#currentDay").text(date);
 	$("#currentTime").text(timeNow);
 
+	localStorage.getItem(event);
+
 	var appointments = [
 		{ timeString: "9AM", timeNumber: 9 },
 		{ timeString: "10AM", timeNumber: 10 },
 		{ timeString: "11AM", timeNumber: 11 },
 		{ timeString: "12PM", timeNumber: 12 },
-		{ timeString: "1PM", timeNumber: 13 },
-		{ timeString: "2PM", timeNumber: 14 },
-		{ timeString: "3AM", timeNumber: 15 },
-		{ timeString: "4PM", timeNumber: 16 },
-		{ timeString: "5PM", timeNumber: 17 },
+		{ timeString: "1PM", timeNumber: 1 },
+		{ timeString: "2PM", timeNumber: 2 },
+		{ timeString: "3PM", timeNumber: 3 },
+		{ timeString: "4PM", timeNumber: 4 },
+		{ timeString: "5PM", timeNumber: 5 },
 	];
 
 	for (let i = 0; i < appointments.length; i++) {
 		var time = appointments[i];
-
+		//Creates div to hold time, appointment, and save button
 		var timeSlot = $("<div>");
 		timeSlot.attr("class", "row");
+
+		//Create column to hold time
 		var timeSpan = $("<span>");
 		timeSpan.attr("class", "hour col-sm-1");
-
 		timeSpan.text(time.timeString);
 		timeSlot.append(timeSpan);
+
+		//Create column to hold appointment ext
 		var textContent = $("<textarea>");
 		textContent.attr("class", "description col-sm-10");
 		timeSlot.append(textContent);
+
+		//Create save button
 		var button = $("<button>");
 		button.attr("class", "saveBtn col-sm-1");
-		button.text("LOCK");
+		button.text("SAVE");
 		timeSlot.append(button);
+
+		//Append entire div to container in html
 		$("#time-block").append(timeSlot);
 
+		//Checks to see current time and adds class accordingly
 		var hourNow = parseInt(time.timeNumber);
+		console.log(hourNow);
 		if (hourNow === parseInt(timeNow)) {
 			textContent.addClass("present");
 		} else if (hourNow <= parseInt(timeNow)) {
@@ -50,17 +61,13 @@ $(document).ready(function () {
 		} else {
 			textContent.addClass("future");
 		}
-	}
 
-	lockEventBtn.on("click", function () {
-		saveEvent();
-	});
-
-	eventDiv.on("click", function () {
-		console.log("eventDiv here");
-	});
-
-	function saveEvent() {
-		console.log("Save event here");
+		$(".description").on("click", function () {
+			console.log("event here!");
+		});
+		$(".saveBtn").on("click", function () {
+			console.log("save here");
+			localStorage.setItem("event", textContent.textContent);
+		});
 	}
 });
